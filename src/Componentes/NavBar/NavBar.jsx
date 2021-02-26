4f9065630e7dca6a9d64f2d0222ea306ec2db5a4
Ivan Ruiz <ivanrur95@hotmail.com>
2021-02-26 00:25:58 -0600

    Se crea carpeta pruebareact, se crea navbar eGamming de prueba con error de imagenes, funcionando a nivel local

---
+++ src/Componentes/NavBar/NavBar.jsx
@@ -0,0 +1,213 @@
+import React from "react";
+
+const navBar = () =>{
+    return (
+        <div>
+                <div class="bar-sale">
+        <div>
+            <span>10% de descuento</span>
+        </div>
+        <div class="email-bar">
+            <a href="mailto:"><i class="fas fa-envelope"></i> Suscribete y gana $100 MXN</a>
+        </div>
+    </div>
+    <div class="serch-bar">
+        <div class="burger-menu">
+            <i class="fas fa-bars"></i>
+        </div>
+        <h1>Egamming</h1>
+    </div>
+    <nav class="menu">
+        <ul>
+            <li><a href="#">PC Gamers <i class="fas fa-chevron-down"></i></a>
+                <div class="mega-menu" id="mega-menu">
+                    <div class="columna">
+                        <ul>
+                            <li><a href="#">Opcion1</a></li>
+                            <li><a href="#">Lorem ipsum dolor sit amet.</a></li>
+                            <li><a href="#">Lorem, ipsum dolor.</a></li>
+                            <li><a href="#">Lorem ipsum dolor sit amet consectetur.</a></li>
+                            <li><a href="#">Lorem, ipsum.</a></li>
+                        </ul>
+                    </div>
+                    <div class="columna">
+                        <ul>
+                            <li><a href="#">Opcion1</a></li>
+                            <li><a href="#">Lorem ipsum dolor sit amet.</a></li>
+                            <li><a href="#">Lorem, ipsum dolor.</a></li>
+                            <li><a href="#">Lorem ipsum dolor sit amet consectetur.</a></li>
+                            <li><a href="#">Lorem, ipsum.</a></li>
+                        </ul>
+                    </div>
+                    <div class="columna">
+                        <ul>
+                            <li><a href="#">Opcion1</a></li>
+                            <li><a href="#">Lorem ipsum dolor sit amet.</a></li>
+                            <li><a href="#">Lorem, ipsum dolor.</a></li>
+                            <li><a href="#">Lorem ipsum dolor sit amet consectetur.</a></li>
+                            <li><a href="#">Lorem, ipsum.</a></li>
+                        </ul>
+                    </div>
+                    <div class="columna">
+                        <ul>
+                            <li><a href="#">Opcion1</a></li>
+                            <li><a href="#">Lorem ipsum dolor sit amet.</a></li>
+                            <li><a href="#">Lorem, ipsum dolor.</a></li>
+                            <li><a href="#">Lorem ipsum dolor sit amet consectetur.</a></li>
+                            <li><a href="#">Lorem, ipsum.</a></li>
+                        </ul>
+                    </div>
+                    <div class="columna">
+                        <ul>
+                            <li><a href="#">Opcion1</a></li>
+                            <li><a href="#">Lorem ipsum dolor sit amet.</a></li>
+                            <li><a href="#">Lorem, ipsum dolor.</a></li>
+                            <li><a href="#">Lorem ipsum dolor sit amet consectetur.</a></li>
+                            <li><a href="#">Lorem, ipsum.</a></li>
+                        </ul>
+                    </div>
+                    <div class="columna">
+                        <ul>
+                            <li><a href="#">Opcion1</a></li>
+                            <li><a href="#">Lorem ipsum dolor sit amet.</a></li>
+                            <li><a href="#">Lorem, ipsum dolor.</a></li>
+                            <li><a href="#">Lorem ipsum dolor sit amet consectetur.</a></li>
+                            <li><a href="#">Lorem, ipsum.</a></li>
+                        </ul>
+                    </div>
+                    <div class="columna">
+                        <ul>
+                            <li><a href="#">Opcion1</a></li>
+                            <li><a href="#">Lorem ipsum dolor sit amet.</a></li>
+                            <li><a href="#">Lorem, ipsum dolor.</a></li>
+                            <li><a href="#">Lorem ipsum dolor sit amet consectetur.</a></li>
+                            <li><a href="#">Lorem, ipsum.</a></li>
+                        </ul>
+                    </div>
+                    <div class="columna-img">
+                        <img src="img/Conquers_550x.jpg" alt="" width="100%"/>
+                    </div>
+                </div>
+            </li>
+
+            <li><a href="#">Pc Streaming <i class="fas fa-chevron-down"></i> </a>
+                <div class="mega-menu" id="mega-menu">
+     
+                    <div class="columna">
+                        <ul>
+                            <li><a href="#">Opcion1</a></li>
+                            <li><a href="#">Lorem ipsum dolor sit amet.</a></li>
+                            <li><a href="#">Lorem, ipsum dolor.</a></li>
+                            <li><a href="#">Lorem ipsum dolor sit amet consectetur.</a></li>
+                            <li><a href="#">Lorem, ipsum.</a></li>
+                        </ul>
+                    </div>
+                    <div class="columna">
+                        <ul>
+                            <li><a href="#">Opcion1</a></li>
+                            <li><a href="#">Lorem ipsum dolor sit amet.</a></li>
+                            <li><a href="#">Lorem, ipsum dolor.</a></li>
+                            <li><a href="#">Lorem ipsum dolor sit amet consectetur.</a></li>
+                            <li><a href="#">Lorem, ipsum.</a></li>
+                        </ul>
+                    </div>
+                    <div class="columna">
+                        <ul>
+                            <li><a href="#">Opcion1</a></li>
+                            <li><a href="#">Lorem ipsum dolor sit amet.</a></li>
+                            <li><a href="#">Lorem, ipsum dolor.</a></li>
+                            <li><a href="#">Lorem ipsum dolor sit amet consectetur.</a></li>
+                            <li><a href="#">Lorem, ipsum.</a></li>
+                        </ul>
+                    </div>
+                    <div class="columna-img">
+                        <img src="img/pc-gammer2.png" alt="" width="100%"/>
+                    </div>
+                </div>
+            </li>
+            <li><a href="#">Estaciones de trabajo <i class="fas fa-chevron-down"></i></a>
+                <div class="mega-menu" id="mega-menu">
+                    <div class="columna">
+                        <ul>
+                            <li><a href="#">Opcion1</a></li>
+                            <li><a href="#">Lorem ipsum dolor sit amet.</a></li>
+                            <li><a href="#">Lorem, ipsum dolor.</a></li>
+                            <li><a href="#">Lorem ipsum dolor sit amet consectetur.</a></li>
+                            <li><a href="#">Lorem, ipsum.</a></li>
+                        </ul>
+                    </div>
+                    <div class="columna">
+                        <ul>
+                            <li><a href="#">Opcion1</a></li>
+                            <li><a href="#">Lorem ipsum dolor sit amet.</a></li>
+                            <li><a href="#">Lorem, ipsum dolor.</a></li>
+                            <li><a href="#">Lorem ipsum dolor sit amet consectetur.</a></li>
+                            <li><a href="#">Lorem, ipsum.</a></li>
+                        </ul>
+                    </div>
+                    <div class="columna">
+                        <ul>
+                            <li><a href="#">Opcion1</a></li>
+                            <li><a href="#">Lorem ipsum dolor sit amet.</a></li>
+                            <li><a href="#">Lorem, ipsum dolor.</a></li>
+                            <li><a href="#">Lorem ipsum dolor sit amet consectetur.</a></li>
+                            <li><a href="#">Lorem, ipsum.</a></li>
+                        </ul>
+                    </div>
+                   
+                    <div class="columna-img">
+                        <img src="img/Conquers_550x.jpg" alt="" width="100%"/>
+                    </div>
+                </div>
+            </li>
+            <li><a href="#">Componentes</a></li>
+            <li><a href="#">Periféricos <i class="fas fa-chevron-down"></i></a>
+               <div class="mega-menu" id="mega-menu">
+                <div class="columna">
+                    <ul>
+                        <li><a href="#">Por tipo de producto</a></li>
+                        <li><a href="#">Monitores</a></li>
+                        <li><a href="#">Silla Gamer</a></li>
+                        <li><a href="#">Headset Gaming</a></li>
+                        <li><a href="#">Mouse</a></li>
+                        <li><a href="#">Teclados</a></li>
+                    </ul>
+                </div>
+                <div class="columna-img"><img src="img/inicio_peris_550x.png" alt="" width="100%"/></div>
+               </div>
+            </li>
+            <li><a href="#">Acercad e Egamming</a>
+                <div class="sub-menu" id="sub-menu">
+                    <div class="columna">
+                        <ul>
+                            <li><a href="#">Noticias</a></li>
+                            <li><a href="#">Colaboraciones</a></li>
+                            <li><a href="#">Reseñas de clientes</a></li>
+                            <li><a href="#">Soporte</a></li>
+                            <li><a href="#">Garantia PREMIER</a></li>
+                            <li><a href="#">Nosotros</a></li>
+                            <li><a href="#">Política de reembolso</a></li>
+                            <li><a href="#">Términos de servicio</a></li>
+                            <li><a href="#">Políticas de envío</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </li>
+
+            <li><a href="#">Mantenimiento</a></li>
+            <li><a href="#">Rastrea tu pedido</a></li>
+        </ul>
+    </nav>
+
+<section>
+    <div class="banner">
+        <img src="img/pf-e3a6c76f--BNNERMRPC45.png" alt=""/>
+    </div>
+</section>
+
+        </div>
+    )
+        
+};
+
+export default navBar;
